d55696a6d3bbf9d3ff5575021a48a3dc6a8c6d17
Cianán <c22393223@mytudublin.ie>
2025-10-30 10:00:57 +0000

    Restart

--- location_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.gis.db import models
-
-class PointOfInterest(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    location = models.PointField()
-
-    def __str__(self):
-        return self.name
