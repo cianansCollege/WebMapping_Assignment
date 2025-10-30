61de34a3ad0143203849f09c77150163b6f5db60
Cianán <c22393223@mytudublin.ie>
2025-10-30 10:48:38 +0000

    Cafe Model and Database Config

--- coffee_stained_map/cafes/models.py
+++ coffee_stained_map/cafes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Cafe(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    location = models.PointField()
+    rating = models.FloatField(default=0.0)
+
+    def _str_(self):
+        return self.name
--- coffee_stained_map/coffee_stained_map/settings.py
+++ coffee_stained_map/coffee_stained_map/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'coffee_stained_map.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'coffee_stained_map',
+        'USER': 'cianan',
+        'PASSWORD': 'awm123',   # your webmapping password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
