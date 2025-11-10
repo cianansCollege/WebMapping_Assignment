7864172db8e2f65754550764192f3f08fefe6977
Cianán <c22393223@mytudublin.ie>
2025-11-10 09:46:06 +0000

    Update tests.py

--- coffee_stained_map/cafes/tests.py
+++ coffee_stained_map/cafes/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.gis.geos import Point
+from cafes.models import Cafe
 
-# Create your tests here.
+class CafeAPITestCase(APITestCase):
+    """Simple API test for the cafes endpoint"""
+
+    def setUp(self):
+        self.cafe = Cafe.objects.create(
+            name="Brew Hub",
+            address="95 Some Street, Dublin",
+            rating=4.5,
+            location=Point(-6.2642, 53.3342, srid=4326)
+        )
+
+    def test_cafe_list_returns_200(self):
+        """Check that the café list endpoint responds successfully"""
+        response = self.client.get("/api/cafes/")  # change to your endpoint if needed
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
