53d2f1facbbefa93fc3c9f8195dd3bd936af0c7c
Cianán <c22393223@mytudublin.ie>
2025-12-06 17:59:10 +0000

    Update settings.py

--- coffee_stained_map/coffee_stained_map/settings.py
+++ coffee_stained_map/coffee_stained_map/settings.py
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'coffee_stained_map.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": os.getenv("DB_NAME", "coffee_map"),
-        "USER": os.getenv("DB_USER", "webmapping"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "coffee_pw"),
-        "HOST": os.getenv("DB_HOST", "db"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
 
 
 
